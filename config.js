8aea9e6a08fd8a21195415e85036d671f9259dd8
Arnoud de Boer <a.deboer@geonovum.nl>
2023-08-30 15:36:42 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var respecConfig = {
-  specStatus: "GN-WV",
+  specStatus: "GN-DEF",
   specType: "IM",
   //canonicalURI: "WV",
-  //publishDate: "2023-05-04",
+  publishDate: "2024-01-01",
   maxTocLevel: 4,
    logos: [ {
     src: 'https://docs.geostandaarden.nl/bro/gen/logo_minbzk.png',
